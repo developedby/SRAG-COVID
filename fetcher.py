907a4b71767db34a304381c52c071e7bd5ccefde
Nicolas Abril <nicolas.abril@gmail.com>
2020-06-18 18:19:55 -0300

    Update p/ dados de 2020-06-16

--- fetcher.py
+++ fetcher.py
@@ -8,18 +8,20 @@ Modo de usar:
     Troque STATE para o estado em que fica o municipio (abreviado e em caps)
     Rode o script (Python 3)
 
+ATENCAO: Os dados de 16/06/2019 vieram com carateres invalidos que precisam ser deletados antes
+
 Autor: Nicolas Abril
-Data de atualização: 10/06/2020
+Data de atualização: 18/06/2020
 """
 
 import csv
 
-FILEPATH = "./INFLUD_09-06-2020.csv"
+FILEPATH = "./INFLUD-16-06-2020-Revisado.csv"
 STATE = 'PR'
 CITYNAME = 'CURITIBA'
 
-file = open(FILEPATH, 'r', newline='', encoding='windows-1252')
-data = list(csv.DictReader(file, delimiter=';', strict=True))
+with open(FILEPATH, 'r', newline='', encoding='windows-1256') as file_cases:
+    data = list(csv.DictReader(file_cases, delimiter=';', strict=True))
 
 cases_sars = 0
 cases_covid = 0
@@ -29,6 +31,7 @@ deaths_respiratory_other = 0
 deaths_other = 0
 deaths_unknown = 0
 deaths_covid = 0
+death_no_cause = 0
 for case in data:
     if case['ID_MUNICIP'] == CITYNAME and case['SG_UF_NOT'] == STATE:
         cases_sars += 1
@@ -38,18 +41,21 @@ for case in data:
 
         if case['EVOLUCAO'] == '2':
             deaths_sars += 1
-            if case['CLASSI_FIN'] == '1':
+            causa_morte = case['CLASSI_FIN']
+            if causa_morte == '1':
                 deaths_influenza += 1
-            elif case['CLASSI_FIN'] == '2':
+            elif causa_morte == '2':
                 deaths_respiratory_other += 1
-            elif case['CLASSI_FIN'] == '3':
+            elif causa_morte == '3':
                 deaths_other += 1
-            elif case['CLASSI_FIN'] == '4':
+            elif causa_morte == '4':
                 deaths_unknown += 1
-            elif case['CLASSI_FIN'] == '5':
+            elif causa_morte == '5':
                 deaths_covid += 1
+            elif causa_morte == '':
+                death_no_cause += 1
             else:
-                print('ue')
+                print(f'Morte por motivo invalido encontrada: {repr(causa_morte)}')
 
 print('Casos SRAG:', cases_sars)
 print('Casos COVID:', cases_covid)
@@ -59,5 +65,4 @@ print('Mortes SRAG outro respiratorio:', deaths_respiratory_other)
 print('Mortes SRAG outro:', deaths_respiratory_other)
 print('Mortes SRAG desconhecido:', deaths_unknown)
 print('Mortes COVID:', deaths_covid)
-
-file.close()
+print('Mortes sem causa registrada:', death_no_cause)
